0f185d0fac53f2ae716751c0e97b49aeb4d8d8b7
waar <1728380670@qq.com>
2023-02-24 21:03:22 +0800

    创建前后端框架项目，实现基本的前后端通信

--- backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -2,14 +2,21 @@ package com.kob.backend.controller.pk;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 @RequestMapping("/pk/")
 public class    IndexController {
 
-    @RequestMapping("index/")
-    public String index()
+    @RequestMapping("getbotinfo/")
+    public Map<String,String> getBot()
     {
-        return "pk/index.html";
+        Map<String,String> map = new HashMap<String, String>();
+        map.put("Bot_Name","zgy");
+        map.put("Bot_age","18");
+        return map;
     }
 }
--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,34 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div>{{user_name}}</div>
+    <div>{{user_age}}</div>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
+<script>
+import $ from "jquery";
+import { ref } from "vue";
+export default {
+  name: "App",
+  setup() {
+    let user_name = ref("");
+    let user_age = ref("");
+    $.ajax({
+      url: "http://localhost:8081/pk/getbotinfo/",
+      type: "get",
+      success(resp) {
+        user_name.value = resp.Bot_Name;
+        user_age.value = resp.Bot_age;
+      },
+    });
+    return {
+      user_age,
+      user_name,
+    };
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
