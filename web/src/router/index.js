197a95e317af0f33da1358688185bd5a7a86f581
waar <1728380670@qq.com>
2023-02-25 17:05:44 +0800

    基本页面跳转

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,12 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import PkIndexView from "@/views/pk/PkIndexView";
+import NotFoundView from "@/views/error/NotFoundView";
+import RanklistIndexView from "@/views/ranklist/RanklistIndexView";
+import RecordIndexView from "@/views/record/RecordIndexView";
+import UserBotIndexView from "@/views/user/bot/UserBotIndexView";
+
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'home',
-  //   component: HomeView
-  // },
-  
+  {
+    path: '/pk/',
+    name: 'pk_index',
+    component: PkIndexView
+  },
+  {
+    path: '/record/',
+    name: 'record_index',
+    component: RecordIndexView
+  },
+  {
+    path: '/userbot/',
+    name: 'userbot_index',
+    component: UserBotIndexView
+  },
+  {
+    path: '/ranklist/',
+    name: 'ranklist_index',
+    component: RanklistIndexView
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: NotFoundView
+  },
+  {
+    path: '/:catchAll(.*)',
+    redirect: '/404',
+  }
+
+
 ]
 
 const router = createRouter({
