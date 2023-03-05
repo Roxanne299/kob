188ac7e3407573132a7c1aa9e9af66e455a706cf
roxanne299 <1728380670@qq.com>
2023-03-05 09:40:57 +0800

    添加了用户登录权限 使用SpringSecurity 密码加密存储

--- backend/src/main/java/com/kob/backend/controller/user/UserController.java
+++ backend/src/main/java/com/kob/backend/controller/user/UserController.java
@@ -1,9 +1,11 @@
 package com.kob.backend.controller.user;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.kob.backend.config.SecurityConfig;
 import com.kob.backend.mapper.UserMapper;
 import com.kob.backend.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,8 +33,15 @@ public class UserController {
 
     @GetMapping("/user/add/{userId}/{username}/{password}")
     public String addUser(@PathVariable int userId,@PathVariable String username,@PathVariable String password){
-        User user = new User(userId,username,password);
+        SecurityConfig securityConfig = new SecurityConfig();
+        PasswordEncoder passwordEncoder = securityConfig.passwordEncoder();
+        User user = new User(userId,username,passwordEncoder.encode(password));
         userMapper.insert(user);
         return "Add success";
+    }
+    @GetMapping("/user/delete/{userId}/")
+    public String deleteById(@PathVariable int userId){
+        userMapper.deleteById(userId);
+        return "Delete success";
     }
 }
